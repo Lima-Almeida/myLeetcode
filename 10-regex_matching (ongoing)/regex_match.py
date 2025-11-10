0f9791a7e54eacd8ca83bcc6d8e36a33b13cef9d
Lima-Almeida <m242827@dac.unicamp.br>
2025-11-10 14:21:06 -0300

    att

--- 10-regex_matching (ongoing)/regex_match.py
+++ 10-regex_matching (ongoing)/regex_match.py
@@ -1,6 +1,6 @@
 
-s = "aa"
-p = "a"
+s = 'aab'
+p = 'c*a*b'
 
 # '.' matches any single character
 # '*' matches zero or more preceding element
@@ -10,11 +10,45 @@ def isMatch(s, p):
     strcomp = list(s)
     pattern = list(p)
 
-    if pattern[0] == '.':
+    if pattern[0] == '.' and pattern[1] == '*':
+        return True
+    
+    if s == p:
+        return True
+
         
-    elif pattern[0] == '*':
-
     else:
+        counter = 0
+        state = False
+        prev_element = ''
+        star_mark = False
+
+        for k in range(len(strcomp)):
+
+            if counter == len(pattern): #wrong
+                if len(strcomp) > len(pattern):
+                    if strcomp[counter] == pattern[counter - 1]:
+                        state = False
+                #state = True
+                break
+
+            if pattern[counter] == '*':
+                star_mark = True
+                prev_element = pattern[counter - 1]
 
 
-    return
+            if not star_mark:
+                if strcomp[k] == pattern[counter] or pattern[counter] == '.':
+                    state = True
+                    counter = counter + 1
+                else:
+                    counter = 0
+            else:
+                if strcomp[k] != prev_element:
+                    counter = 0
+                    star_mark = False
+
+
+    return state
+
+print(isMatch(s, p))
--- 8-str_to_int (ongoing)/str_to_int.py
+++ 8-str_to_int (ongoing)/str_to_int.py
@@ -1,4 +1,33 @@
+s = '-91283472332'
 
 
 def myAtoi(s):
-    return
+    str_list = list(s)
+    aux_list = []
+    added_sign = False
+
+    while True:
+        if str_list[0] == ' ' or str_list[0] == '+':
+            del str_list[0]
+        if str_list[0] == '-' and not added_sign:
+            added_sign = True
+            aux_list.append('-')
+            del str_list[0]
+        if 47 < ord(str_list[0]) and ord(str_list[0]) < 58:
+            break
+        elif str_list[0] != '-' and str_list[0] != ' ':
+            return 0
+        
+    print(aux_list)
+
+    for k in str_list:
+        if 47 < ord(k) and ord(k) < 58:
+            aux_list.append(k)
+        else:
+            break
+
+    print(aux_list)
+
+    return int("".join(aux_list))
+
+print(myAtoi(s))
